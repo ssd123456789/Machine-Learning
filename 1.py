2f442d3127412ba00fceb4befef351cc14ae0f82
unknown <dr.shrutidesai21@gmail.com>
2022-08-07 22:35:17 +0530

    new change

--- 1.py
+++ 1.py
@@ -1,13 +1,1 @@
 import pandas as pd
-import numpy as np
-
-df=pd.read_csv("axis.csv")
-dthsezrg
-jkln.k;
-hulhuop
-j;hiko;
-eWFCWE
-111111111111111111111
-2222222222222222
-333333333333333
-4444444444444444444444
